238a68e7457d1b1a8f402fe2c1bf9e659f8ed13c
stefkwan-h8 <82027557+stefkwan-h8@users.noreply.github.com>
2022-10-19 11:49:47 +0700

    sesi 2

---
+++ andi.py
@@ -0,0 +1,7 @@
+nama_depan = "Andhika"
+nama_belakang = "Widjaja"
+nama_lengkap = nama_depan + " " + nama_belakang
+hobi = ["memasak", "bulutangkis", "sepeda"]
+
+def menyapa():
+  print("salam kenal saya Andi!")
---
+++ budi.py
@@ -0,0 +1,10 @@
+nama = "Budi Budiman"
+hobi = ["merajut", "coding", "melukis"]
+umur = 60
+
+def menyapa():
+  print("Perkenalkan saya Budi.")
+
+def marah(kenapa):
+  print("HEY!")
+  print("Tidak Boleh", kenapa)
